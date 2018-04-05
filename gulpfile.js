a39ba52c8bdb07de015acb5cda6af7174efbae76
Alexandr <alex.drag92@yandex.ru>
2018-04-06 02:27:24 +0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var bs = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+  return gulp.src('source/sass/style.scss')
+         .pipe(sass({
+            style : 'compressed'
+          }))
+         .pipe(gulp.dest('source/css'))
+         .pipe(bs.stream())
+});
+
+
+gulp.task('serve', ['sass'], function(){
+  bs.init({
+    server: 'source/'
+  });
+
+  gulp.watch('source/sass/**/*.scss', ['sass']);
+  gulp.watch('source/*.html').on('change', bs.reload);
+});
